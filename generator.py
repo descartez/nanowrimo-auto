104ca02b05f539e121ead291e9e2850f14d02ae7
Ryan Stong <rvanstong@gmail.com>
2017-11-01 10:00:13 -0700

    stubs out create chapter method

--- generator.py
+++ generator.py
@@ -19,14 +19,9 @@ class POSifiedText(markovify.Text):
 #------------------------------------------
 
 # Creates a "character" class, that can load in mulitple files.
-class Character:
+class Text:
     def __init__(self):
-        self.name = ""
-        self.bio = ""
-
-    def create_bio(self, name, bio):
-        self.name = name
-        self.bio = bio
+        self.model = None
 
     def create_model(self, file):
         with open(file) as f:
@@ -45,38 +40,4 @@ class Character:
                         combined_model = model
         self.model = combined_model
 
-    def speak_bio(self):
-        print("-"*15)
-        print(self.name)
-        print(self.bio)
-        print("-"*15)
-
-    def speak_tweet(self):
-        print(self.model.make_short_sentence(140))
-
-    def return_tweet(self):
-        return self.model.make_short_sentence(140)
-
-class CharacterChooser:
-    def __init__(self):
-        self.models = []
-
-
-
-lovecraft = Character()
-lovecraft.create_bio("H.P. Lovecraft", "A xenophobic creepy writer.")
-lovecraft.create_model_from_dir("character_texts/lovecraft")
-lovecraft.speak_bio()
-
-tweet = lovecraft.return_tweet()
-print(tweet)
-api.update_status(tweet)
-
-gaiman = Character()
-gaiman.create_bio("Neil Gaiman", "An excellent, empathetic man and writer.")
-gaiman.create_model_from_dir("character_texts/gaiman")
-gaiman.speak_bio()
-
-tweet = gaiman.return_tweet()
-print(tweet)
-api.update_status(tweet)
+    def create_chaper(self, number_of_chapters):
